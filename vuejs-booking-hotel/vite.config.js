03f8b2ceaf292b7d73a2acb156534adb18b0c0f7
tuanba <tuanbui.10103@gmail.com>
2025-05-26 11:20:35 +0700

    update: update vite config

--- vuejs-booking-hotel/vite.config.js
+++ vuejs-booking-hotel/vite.config.js
@@ -1,18 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.js"],
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+});
